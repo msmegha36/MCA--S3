4d815cc07d69df2c7fcecc411d09fad6cd581fb8
msmegha36 <chn24mc060@ceconline.edu>
2025-09-13 08:10:22 +0530

    Cycle 3 completed

---
+++ MAD/CYCLE-3/arrayAdapter/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.newarrayadapter;
+
+import static com.example.newarrayadapter.R.id.listView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        ListView listView=findViewById(R.id.listView);
+
+        String [] fruits={"Apple","Banana","Cherry","Dates","Grape","Kiwi","Lemon","Mnago","Orange","Peach"};
+
+        ArrayAdapter<String>adapter=new ArrayAdapter<>(this
+                ,android.R.layout.simple_list_item_1,fruits);
+        listView.setAdapter(adapter);
+    }
+}
